5be7ea21826b0db84aac14f1c18e7c004675d14d
kartikeyadhingra <you@example.com>
2021-01-16 17:56:21 -0500

    commented

--- UTEK 2021/1a.py
+++ UTEK 2021/1a.py
@@ -1,13 +1,13 @@
 import jsonFunctions
 
-def output1a(accessibleStations):
+def output1a(accessibleStations):                                           # output function to write th output to a .out file
     f = open("1a.out", "w")
     f.write(', '.join(accessibleStations))
     f.close()
 
 def main():
-    stations = jsonFunctions.extractJson('1a.json','Nodes')
-    accessibleStations = jsonFunctions.returnAccessibleStations(stations)
-    output1a(accessibleStations)
+    stations = jsonFunctions.extractJson('1a.json','Nodes')                 # extract the Json file (input)
+    accessibleStations = jsonFunctions.returnAccessibleStations(stations)   # use the return Accessible functions to get the list of stations that are accessible
+    output1a(accessibleStations)                                            # generate the output file
 
 main()
--- UTEK 2021/1b.py
+++ UTEK 2021/1b.py
@@ -3,6 +3,12 @@ from jsonFunctions import extractJson
 from sortingFunctions import mergesort
 
 def find_k_most_accesible(inputFile:str,k:int):
+    '''
+    Inputs:
+        inputFile: file name of the input, k: an integer for the number of results to be displayed
+    Returns
+        k most accessible paths as a list
+    '''
     list_paths = extractJson(inputFile,'Paths')
 
     lst_out = []
@@ -12,13 +18,13 @@ def find_k_most_accesible(inputFile:str,k:int):
         for node in path["Nodes"]:
             if node["Accessible"]:
                 accessible+=1
-        lst_out.append(tuple((path["PathName"], accessible/(len(path["Nodes"])-accessible))))
+        lst_out.append(tuple((path["PathName"], accessible/(len(path["Nodes"])-accessible))))               # appends a tuple consisting of the name of the path and the accessibility ratio of that path
 
     kMostAccessible = mergesort(lst_out)[-k:]
     kMostAccessible.reverse()
-    return kMostAccessible
+    return kMostAccessible                                                                                  # Returns a list of k paths that are in decreasing order of the accesibility ratio.
 
-def output1b(lst_out):
+def output1b(lst_out):                                                                                      # Writes the output to the .out file
     f = open("1b.out", "w")
     f.write(', '.join(lst_out))
     f.close()
