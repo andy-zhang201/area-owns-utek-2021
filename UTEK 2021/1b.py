b80211912839ebf753cd3209877b273258c2f317
kartikeyadhingra <you@example.com>
2021-01-16 14:49:57 -0500

    1b v0.1

--- UTEK 2021/1b.py
+++ UTEK 2021/1b.py
@@ -7,11 +7,12 @@ list_paths = json.load(file_1b)["Paths"]
 lst_out = []
 
 for path in list_paths:
-    num_of_accessible_stations = rAS(path["Nodes"])
     accessible=0
-    for node in num_of_accessible_stations:
-        total+=1
+    for node in path["Nodes"]:
         if node["Accessible"]:
             accessible+=1
+    lst_out.append(tuple(path["PathName"], accessible/(len(path["Nodes"]))
+
+print(type(lst_out))
 
 
